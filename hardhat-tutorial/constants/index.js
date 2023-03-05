e2f68d129edf5202c04f183eae529c7ad6434073
Greg Delgado <grd888@gmail.com>
2023-03-05 14:45:47 +0800

    create deploy script

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+WHITELIST_CONTRACT_ADDRESS = '0x8c4E1101Ce32968AEfD5F466a1a7d50daBF382E7';
+METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
+
+// deployed contract address: 0xDd63062aeEd5B7493C09664369C177653855FE8B
