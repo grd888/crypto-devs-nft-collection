bafbcb52bc411d3bce4d245ff4743f638e4feef6
Greg Delgado <grd888@gmail.com>
2023-03-05 14:11:41 +0800

    Create Whitelist interface and CryptoDevs base contract

---
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    /**
+     * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     */
+    string _baseTokenURI;
+
+    // Whitelist contract instance
+    IWhitelist whitelist;
+
+    /**
+     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+     * name in our case is `Crypto Devs` and symbol is `CD`.
+     * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+     * It also initializes an instance of whitelist interface.
+     */
+    constructor(
+        string memory baseURI,
+        address whitelistContract
+    ) ERC721("Crypto Devs", "CD") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+}
