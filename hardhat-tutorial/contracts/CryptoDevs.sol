1e147de980cfc81fb5bb54b0fc0626f97533150e
Greg Delgado <grd888@gmail.com>
2023-03-05 14:14:42 +0800

    create startPresale() function

--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -15,6 +15,12 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     // Whitelist contract instance
     IWhitelist whitelist;
 
+    // boolean to keep track of whether presale started or not
+    bool public presaleStarted;
+
+    // timestamp for when presale would end
+    uint256 public presaleEnded;
+
     /**
      * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
      * name in our case is `Crypto Devs` and symbol is `CD`.
@@ -27,5 +33,15 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     ) ERC721("Crypto Devs", "CD") {
         _baseTokenURI = baseURI;
         whitelist = IWhitelist(whitelistContract);
+    }
+
+    /**
+    * @dev startPresale starts a presale for the whitelisted addresses
+      */
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        // Set presaleEnded time as current timestamp + 2 days
+        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
+        presaleEnded = block.timestamp + 2 days;
     }
 }
